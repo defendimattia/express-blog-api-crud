b27773589cb45da66695bfca2dabbf64a3e0e409
defendimattia <defendi.mattia92@gmail.com>
2025-03-28 14:46:22 +0100

    add: routers js and folder + import export

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const postsRouter = require("./routers/posts-router")
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
