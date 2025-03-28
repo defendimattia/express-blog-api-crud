cb9443625dd746960bf27b716f26a5674daec929
defendimattia <defendi.mattia92@gmail.com>
2025-03-28 14:54:46 +0100

    add: posts routes

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -1,4 +1,28 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.send("Posts list")
+})
+
+router.get("/:id", (req, res) => {
+    res.send(`Post ${req.params.id} details`)
+})
+
+router.post("/", (req, res) => {
+    res.send("Created new post")
+})
+
+router.put("/:id", (req, res) => {
+    res.send(`Post ${req.params.id} full edit`)
+})
+
+router.patch("/:id", (req, res) => {
+    res.send(`Post ${req.params.id} partial edit`)
+})
+
+router.delete("/:id", (req, res) => {
+    res.send(`Post ${req.params.id} deleted`)
+})
+
 module.exports = router
