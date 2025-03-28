34303c7cdb2bcaf6ccfb07a4ac34748a3db7a557
defendimattia <defendi.mattia92@gmail.com>
2025-03-28 14:48:25 +0100

    add: app.use("/posts")

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express()
 const port = 3000
 const postsRouter = require("./routers/posts-router")
 
+app.use("/posts", postsRouter)
+
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
