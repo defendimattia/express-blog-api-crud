998692c2aa5f25056506022fd42ab838a7adf756
defendimattia <defendi.mattia92@gmail.com>
2025-04-01 12:45:24 +0200

    add: middlevare "notFound"

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ const port = 3000
 // importa il router dei post
 const postsRouter = require("./routers/posts-router")
 
+// importa middleware
+const notFound = require("./middleware/not-found")
+
+
 // middleware body-parser json
 app.use(express.json())
 
 // assegna il router dei post con la rotta "/posts"
 app.use("/posts", postsRouter)
 
-
+// gestisce le rotte non trovate
+app.use(notFound)
 
 
 // avvia il server e mostra il messaggio in console
